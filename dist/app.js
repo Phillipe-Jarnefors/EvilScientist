b98dbdc8a24afd59fb387e8c170d6079bb88c07b
Phillipe Jarnefors <phillipe_jarnefors@hotmail.com>
2023-01-13 11:29:25 +0100

    Pushed global functions into object

--- dist/app.js
+++ dist/app.js
@@ -3,74 +3,81 @@ const evilScientistList = document.querySelector(".evil-scientist-list");
 const cardShow = document.querySelector(".card-show");
 const formData = document.querySelector(".form-data");
 const submitForm = document.querySelector("#submit-form");
-const evilScientists = [
-    {
-        name: "Klaus Benedict",
-        age: 50,
-        henchmen: 12,
-        desc: "Built Frankensteins Monster more advanced",
-    },
-    {
-        name: "Wilhelm Will",
-        age: 40,
-        henchmen: 0,
-        desc: "Lone Wolf",
-    },
-    {
-        name: "Pingis Khan",
-        age: 60,
-        henchmen: 100,
-        desc: "Wish to rule the world",
-    },
-];
-function printListOfScientists() {
-    evilScientistList.innerHTML = '';
-    let evilScientistsLength = evilScientists.length;
-    for (let i = 0; i < evilScientistsLength; i++) {
-        let divElem = document.createElement('div');
-        let h2Elem = document.createElement('h2');
-        divElem.className = "card";
-        h2Elem.className = "evil-name";
-        h2Elem.innerHTML = `${evilScientists[i].name}`;
-        evilScientistList.append(divElem);
-        divElem.append(h2Elem);
-        divElem.addEventListener("click", (event) => {
-            event.preventDefault();
-            cardShow.innerHTML = "";
-            let pickedEvil = h2Elem.innerText;
-            let evilArray = [];
-            const choosenEvil = evilScientists.find(evil => evil.name === pickedEvil);
-            for (const [key, value] of Object.entries(choosenEvil)) {
-                let evilString = '';
-                evilString += key + ': ' + value;
-                evilArray.push(evilString);
-            }
-            let prettyArray = evilArray.map(letter => letter.charAt(0).toUpperCase() + letter.substring(1));
-            prettyArray.forEach(element => {
-                const listElement = document.createElement('li');
-                const ulElement = document.createElement('ul');
-                listElement.innerHTML = element;
-                ulElement.append(listElement);
-                cardShow.append(ulElement);
+let evilObject = {
+    evilScientists: [
+        {
+            name: "Klaus Benedict",
+            age: 50,
+            henchmen: 12,
+            desc: "Built Frankensteins Monster more advanced",
+        },
+        {
+            name: "Wilhelm Will",
+            age: 40,
+            henchmen: 0,
+            desc: "Lone Wolf",
+        },
+        {
+            name: "Pingis Khan",
+            age: 60,
+            henchmen: 100,
+            desc: "Wish to rule the world",
+        },
+    ],
+    printListOfScientists() {
+        evilScientistList.innerHTML = '';
+        let evilScientistsLength = this.evilScientists.length;
+        for (let i = 0; i < evilScientistsLength; i++) {
+            let divElem = document.createElement('div');
+            let h2Elem = document.createElement('h2');
+            divElem.className = "card";
+            h2Elem.className = "evil-name";
+            h2Elem.innerHTML = `${this.evilScientists[i].name}`;
+            evilScientistList.append(divElem);
+            divElem.append(h2Elem);
+            divElem.addEventListener("click", (event) => {
+                event.preventDefault();
+                cardShow.innerHTML = "";
+                let pickedEvil = h2Elem.innerText;
+                let evilArray = [];
+                const choosenEvil = this.evilScientists.find(evil => evil.name === pickedEvil);
+                for (const [key, value] of Object.entries(choosenEvil)) {
+                    let evilString = '';
+                    evilString += key + ': ' + value;
+                    evilArray.push(evilString);
+                }
+                ;
+                let prettyArray = evilArray.map(letter => letter.charAt(0).toUpperCase() + letter.substring(1));
+                prettyArray.forEach(element => {
+                    const listElement = document.createElement('li');
+                    const ulElement = document.createElement('ul');
+                    listElement.innerHTML = element;
+                    ulElement.append(listElement);
+                    cardShow.append(ulElement);
+                });
             });
-        });
-    }
-}
-printListOfScientists();
+        }
+        ;
+    },
+    addValueFromForm() {
+        const evilFullName = document.querySelector("#fullname").value;
+        const evilAge = document.querySelector("#age").valueAsNumber;
+        const evilHenchmen = document.querySelector("#henchmen").valueAsNumber;
+        const evilDesc = document.querySelector("#desc").value;
+        const evilArr = {
+            name: evilFullName,
+            age: evilAge,
+            henchmen: evilHenchmen,
+            desc: evilDesc,
+        };
+        this.evilScientists.push(evilArr);
+        this.printListOfScientists();
+        const resetForm = document.querySelector('.form');
+        resetForm.reset();
+    },
+};
 submitForm.addEventListener('click', function (event) {
     event.preventDefault();
-    const evilFullName = document.querySelector("#fullname").value;
-    const evilAge = document.querySelector("#age").valueAsNumber;
-    const evilHenchmen = document.querySelector("#henchmen").valueAsNumber;
-    const evilDesc = document.querySelector("#desc").value;
-    const evilArr = {
-        name: evilFullName,
-        age: evilAge,
-        henchmen: evilHenchmen,
-        desc: evilDesc,
-    };
-    evilScientists.push(evilArr);
-    printListOfScientists();
-    const resetForm = document.querySelector('.form');
-    resetForm.reset();
+    evilObject.addValueFromForm();
 });
+evilObject.printListOfScientists();
--- src/app.ts
+++ src/app.ts
@@ -10,91 +10,98 @@ type Evils = {
 	desc: string,
 }
 
-const evilScientists: Evils[] = [
-	{
-		name: "Klaus Benedict",
-		age: 50, 
-		henchmen: 12,
-		desc: "Built Frankensteins Monster more advanced",
+let evilObject: {
+	evilScientists: Evils[],
+	printListOfScientists: ()=> void,
+	addValueFromForm: ()=> void,
+} = {	
+		evilScientists: [
+		{
+			name: "Klaus Benedict",
+			age: 50, 
+			henchmen: 12,
+			desc: "Built Frankensteins Monster more advanced",
+		},
+		{
+			name: "Wilhelm Will",
+			age: 40, 
+			henchmen: 0,
+			desc: "Lone Wolf",
+		},
+		{
+			name: "Pingis Khan",
+			age: 60, 
+			henchmen: 100,
+			desc: "Wish to rule the world",
+		},
+	],
+	// ==== Show List of Evil ====
+	printListOfScientists(): void {
+		evilScientistList.innerHTML = '';
+		let evilScientistsLength: number = this.evilScientists.length;
+		for (let i = 0; i < evilScientistsLength; i++) {
+			let divElem = document.createElement('div') as HTMLDivElement;
+			   let h2Elem = document.createElement('h2') as HTMLHeadElement;
+			divElem.className = "card";
+			h2Elem.className = "evil-name";
+			h2Elem.innerHTML = `${this.evilScientists[i].name}`;
+			evilScientistList.append(divElem);
+			divElem.append(h2Elem);
+	
+			/// ==== When click on Evil, compare innertext and find the object ====
+			divElem.addEventListener("click", (event) => {
+				event.preventDefault();
+				cardShow.innerHTML = "";
+				let pickedEvil: string = h2Elem.innerText;
+				let evilArray: string[] = [];
+				const choosenEvil = this.evilScientists.find(evil => evil.name === pickedEvil) as Evils;
+				
+				// ==== When match accours. Open key/value -pair and pretty-print it ====
+				for (const [key, value] of Object.entries(choosenEvil)) {
+					let evilString: string = '';
+					evilString += key + ': ' + value;
+					evilArray.push(evilString);
+				};
+				let prettyArray: string[] = evilArray.map(letter => letter.charAt(0).toUpperCase() + letter.substring(1));
+				
+				// ==== Create ul/li and append to div ====
+				prettyArray.forEach(element => {
+					const listElement = document.createElement('li') as HTMLElement;
+					const ulElement = document.createElement('ul') as HTMLElement;
+					listElement.innerHTML = element;
+					ulElement.append(listElement);
+					cardShow.append(ulElement);
+				});
+			});
+		};
 	},
-	{
-		name: "Wilhelm Will",
-		age: 40, 
-		henchmen: 0,
-		desc: "Lone Wolf",
+	addValueFromForm(): void {
+		const evilFullName: string = (document.querySelector("#fullname") as HTMLInputElement).value;
+		const evilAge: number = (document.querySelector("#age") as HTMLInputElement).valueAsNumber;
+		const evilHenchmen: number = (document.querySelector("#henchmen") as HTMLInputElement).valueAsNumber;
+		const evilDesc: string = (document.querySelector("#desc") as HTMLInputElement).value;
+		
+		const evilArr: Evils = {
+			name: evilFullName,
+			age: evilAge,
+			henchmen: evilHenchmen,
+			desc: evilDesc,
+		};
+		
+		this.evilScientists.push(evilArr);
+		this.printListOfScientists();
+		
+		const resetForm = document.querySelector('.form') as HTMLFormElement;
+		resetForm.reset();
 	},
-	{
-		name: "Pingis Khan",
-		age: 60, 
-		henchmen: 100,
-		desc: "Wish to rule the world",
-	},
-]
+};
 
-// ==== Show List of Evil ====
-function printListOfScientists(): void {
-	evilScientistList.innerHTML = '';
-	let evilScientistsLength: number = evilScientists.length;
-	for (let i = 0; i < evilScientistsLength; i++) {
-		let divElem = document.createElement('div') as HTMLDivElement;
-   		let h2Elem = document.createElement('h2') as HTMLHeadElement;
-		divElem.className = "card";
-		h2Elem.className = "evil-name";
-		h2Elem.innerHTML = `${evilScientists[i].name}`;
-		evilScientistList.append(divElem);
-		divElem.append(h2Elem);
-
-		/// ==== When click on Evil, compare innertext and find the object ====
-		divElem.addEventListener("click", (event) => {
-			event.preventDefault();
-			cardShow.innerHTML = "";
-			let pickedEvil: string = h2Elem.innerText;
-			let evilArray: string[] = [];
-			const choosenEvil = evilScientists.find(evil => evil.name === pickedEvil) as Evils;
-			
-			// ==== When match accours. Open key/value -pair and pretty-print it ====
-			for (const [key, value] of Object.entries(choosenEvil)) {
-				let evilString: string = '';
-				evilString += key + ': ' + value;
-				evilArray.push(evilString);
-			}
-			let prettyArray: string[] = evilArray.map(letter => letter.charAt(0).toUpperCase() + letter.substring(1));
-			
-			// ==== Create ul/li and append to div ====
-			prettyArray.forEach(element => {
-				const listElement = document.createElement('li') as HTMLElement;
-				const ulElement = document.createElement('ul') as HTMLElement;
-				listElement.innerHTML = element;
-				ulElement.append(listElement);
-				cardShow.append(ulElement);
-			})
-		})
-	}
-}
-printListOfScientists();
-
-// ==== Get right value from input inside form ====
 submitForm.addEventListener('click', function(event){
 	event.preventDefault();
-	const evilFullName: string = (document.querySelector("#fullname") as HTMLInputElement).value;
-	const evilAge: number = (document.querySelector("#age") as HTMLInputElement).valueAsNumber;
-	const evilHenchmen: number = (document.querySelector("#henchmen") as HTMLInputElement).valueAsNumber;
-	const evilDesc: string = (document.querySelector("#desc") as HTMLInputElement).value;
-
-	const evilArr: Evils = {
-		name: evilFullName,
-		age: evilAge,
-		henchmen: evilHenchmen,
-		desc: evilDesc,
-	}
-
-	evilScientists.push(evilArr);
-	printListOfScientists();
-
-	const resetForm = document.querySelector('.form') as HTMLFormElement;
-	resetForm.reset();
+	evilObject.addValueFromForm();
 })
 
+evilObject.printListOfScientists();
 
 
 
